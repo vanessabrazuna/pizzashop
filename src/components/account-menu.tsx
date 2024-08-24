3106781e6537b7af44de9c1434310683c73aad0f
vanessabrazuna <brazuna.nessa@gmail.com>
2024-08-24 20:24:04 -0300

    feat: Add StoreProfileDialog

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,6 +1,6 @@
-import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { Building, ChevronDown, LogOut } from "lucide-react"
 
-import { Button } from './ui/button'
+import { Button } from "./ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,37 +8,78 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
+} from "./ui/dropdown-menu"
+import { useQuery } from "@tanstack/react-query"
+import { getProfile } from "@/api/get-profile"
+import { getManagedRestaurant } from "@/api/get-managed-restaurant"
+import { Skeleton } from "./ui/skeleton"
+import { Dialog, DialogTrigger } from "./ui/dialog"
+import { StoreProfileDialog } from "./store-profile-dialog"
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaurant,
+    })
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex select-none items-center gap-2"
-        >
-          Pizza Shop
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          <span>Vanessa Brazuna</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            teste.test@gmail.com
-          </span>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}{" "}
+            Pizza Shop
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>
+                  {profile?.name}
+                  TesteName
+                </span>
+                <span className="text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                  teste.teste@email.com
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,16 @@
-import axios from 'axios'
+import axios from "axios"
 
-import { env } from '@/env'
+import { env } from "@/env"
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 })
+
+// if (env.VITE_ENABLE_API_DELAY) {
+//   api.interceptors.request.use(async (config) => {
+//     await new Promise((resolve) => setTimeout(() => resolve, 100))
+
+//     return config
+//   })
+// }
