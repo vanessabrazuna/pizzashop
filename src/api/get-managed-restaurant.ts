1172e847f77f771cd17581e63e5c17053444005e
vanessabrazuna <brazuna.nessa@gmail.com>
2024-12-22 19:16:58 -0300

    test: Setup do PlayWright, testes E2E and Async Assertions

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -3,7 +3,7 @@ import { api } from "@/lib/axios"
 export interface GetManagedRestaurantResponse {
   id: string
   name: string
-  createAt: Date | null
+  createAt: Date
   updateAt: Date | null
   description: string | null
   managerId: string | null
